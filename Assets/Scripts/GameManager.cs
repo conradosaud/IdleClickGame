6a7fb55a08a6b70c5e480da57fc2764889ae3877
Conrado Saud <50385963+conradosaud@users.noreply.github.com>
2024-04-12 17:40:34 -0300

    Save funcionando

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using TMPro;
 using UnityEngine;
 
@@ -26,6 +27,8 @@ public class GameManager : MonoBehaviour
     {
         // Pega o texto de dinheiro que está na tela
         textoDinheiro = GameObject.Find("Canvas").transform.Find("Dinheiro").GetComponent<TextMeshProUGUI>();
+
+        AutoSave();
     }
 
     // A variável de dinheiro poderia ser alterada diretamente, porém ao usar esta função,
@@ -36,4 +39,60 @@ public class GameManager : MonoBehaviour
         textoDinheiro.text = "$ " + GameManager.dinheiro.ToString();
     }
 
+    // -----------------------------------
+
+    public void SalvarJogo()
+    {
+
+        DadosJogo dj = new DadosJogo();
+        dj.dinheiro = dinheiro;
+        dj.clicadores = clicadores;
+        dj.multiplicadores = multiplicadores;
+
+        string json = JsonUtility.ToJson(dj);
+        string nomeArquivo = "save_data.json";
+        string pasta = Application.persistentDataPath;
+        string caminho = pasta + Path.AltDirectorySeparatorChar + nomeArquivo;
+
+        File.WriteAllText(caminho, json);
+
+    }
+
+    void AutoSave()
+    {
+        SalvarJogo();
+        Invoke("AutoSave", 5);
+    }
+
+
+    void CarregarJogo()
+    {
+        string nomeArquivo = "save_data.json";
+        string pasta = Application.persistentDataPath;
+        string caminho = pasta + Path.AltDirectorySeparatorChar + nomeArquivo;
+
+        string dados = null;
+        if( File.Exists(caminho))
+        {
+            dados = File.ReadAllText(caminho);
+        }
+
+        DadosJogo dj = JsonUtility.FromJson<DadosJogo>(dados);
+
+        dinheiro = dj.dinheiro;
+        clicadores = dj.clicadores;
+        multiplicadores = dj.multiplicadores;
+
+        // PlayerPrefs.DeleteAll();
+        // File.Delete(caminho);
+
+    }
+
+}
+
+class DadosJogo
+{
+    public int dinheiro;
+    public int clicadores;
+    public int multiplicadores;
 }
