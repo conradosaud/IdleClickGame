34a81105c847daa5593c1c68d80be8702a62b995
Golembr27 <142447072+Golembr27@users.noreply.github.com>
2024-04-12 16:21:52 -0300

    PlayerPrefsFuncionando

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using System;
 
 /// <summary>
 ///     Gerencia todos os registros do jogo, como a quantidade de dinheiro, clicadores e multiplicadores
@@ -24,8 +25,22 @@ public class GameManager : MonoBehaviour
     // texto do dinheiro exista para funcionar. Então deve ser iniciado com certeza antes de todos
     private void Awake()
     {
+        CarregarJogo();
         // Pega o texto de dinheiro que está na tela
         textoDinheiro = GameObject.Find("Canvas").transform.Find("Dinheiro").GetComponent<TextMeshProUGUI>();
+        
+        if (ExisteSave())
+        {
+            custoClicador = clicadores * (int)Math.Floor(custoClicador * 1.25f);
+            custoMultiplicador = multiplicadores * (int)Math.Floor(custoMultiplicador * 1.25f);
+        }
+
+        AutoSave();
+    }
+
+    private void OnApplicationQuit()
+    {
+        AutoSave();
     }
 
     // A variável de dinheiro poderia ser alterada diretamente, porém ao usar esta função,
@@ -36,4 +51,46 @@ public class GameManager : MonoBehaviour
         textoDinheiro.text = "$ " + GameManager.dinheiro.ToString();
     }
 
+    // ---------------
+
+    public static void SalvarJogo()
+    {
+        PlayerPrefs.SetInt("dinheiro",dinheiro);
+        PlayerPrefs.SetInt("clicador", clicadores);
+        PlayerPrefs.SetInt("multiplicadores", multiplicadores);
+        PlayerPrefs.Save();
+        ToastNotification.Show("Jogo Salvo com sucesso!");
+    }
+
+    public void CarregarJogo()
+    {
+        if (ExisteSave() == false)
+        {
+            return;
+        }
+
+        dinheiro = PlayerPrefs.GetInt("dinheio");
+        clicadores = PlayerPrefs.GetInt("clicador");
+        multiplicadores = PlayerPrefs.GetInt("multiplicadores");
+    }
+
+    //Função com retorno
+    bool ExisteSave()
+    {
+        if (PlayerPrefs.HasKey("dinheiro") == true)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+        
+    }
+
+    void AutoSave()
+    {
+        SalvarJogo();
+        Invoke("AutoSave", 5);
+    }
 }
